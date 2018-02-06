24131dd597c7331f14fb377646210eda03df97eb
Romain niam0r <niamor@mm.st>
2018-02-06 15:45:12 +0000

    first flat displaying

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './components/hello';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
